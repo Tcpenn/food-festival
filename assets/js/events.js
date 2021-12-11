ab04d0792406061f6110f9ddd1625eed7bab8ef1
Tcpenn <tcpenn1026@gmail.com>
2021-12-11 03:41:19 -0500

    added file loads and seperated files

---
+++ assets/js/events.js
@@ -0,0 +1,10 @@
+require("bootstrap");
+const createEl = require("./domMethods");
+const createLoremIpsum = require("./helpers");
+
+
+if (window.location.href.indexOf("event") > -1) {
+    $(document).ready( function() {
+        // DOM manipulation code specific to each page.
+        })
+}
---
+++ assets/js/schedule.js
@@ -0,0 +1,10 @@
+require("bootstrap");
+const createEl = require("./domMethods");
+const { createLoremIpsum, dateConverter } = require("./helpers");
+
+
+if (window.location.href.indexOf("schedule") > -1) {
+    $(document).ready( function() {
+        // DOM manipulation code specific to each page.
+        })
+}
